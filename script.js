93b37d798904d6b2cc95f52ebb704fc5df0d223d
lepions <45270812+lepions@users.noreply.github.com>
2023-04-23 13:33:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let btn = document.getElementById('time')
 
 function calcul1(){
 	var today = new Date();
-	let jour = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"]
+	let jour = ["Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"]
 	let mois = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"]
 	var date = 'Nous sommes le : '+ jour[today.getDay()]+ " " +today.getDate()+ " " +mois[(today.getMonth())]+ " " +today.getFullYear();
 	alert(date);
